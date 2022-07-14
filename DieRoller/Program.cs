8ec24325c08c7a93d4a37d1d1c82500af641ffe7
JoeLehmanJr <joejr65@live.com>
2022-07-13 18:49:43 -0700

    Reedited the test code.

--- DieRoller/Program.cs
+++ DieRoller/Program.cs
@@ -1,8 +1,20 @@
-﻿using DieRoller;
+﻿
+using DieRoller;
 
-Die myDie = new Die();
-myDie.FaceValue = 6;
-byte currentValue = myDie.FaceValue;
+Die myDie = new();
+Console.WriteLine(myDie.FaceValue);
 myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+
+myDie.IsHeld = true;
+Console.WriteLine("Holding the die*****");
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
 
 Console.ReadKey();
